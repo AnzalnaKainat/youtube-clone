2e0984112e38e397ab8379d0817de4bb1707d190
AnzalnaKainat <anzalnakainat@gmail.com>
2024-07-28 18:55:14 +0500

    second commit

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -29,3 +29,56 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
 );
 
 export default VideoCard
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { Link } from "react-router-dom"; 
+// import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+// import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+// import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+// const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
+//   <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 350, boxShadow: 'none', borderRadius: 0 }}>
+//     <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+//       <CardMedia 
+//         image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
+//         alt={snippet?.title || demoVideoTitle}
+//         sx={{ width: '100%', height: 180 }} 
+//       />
+//     </Link>
+//     <CardContent sx={{ backgroundColor: '#1E1E1E', height: 170 }}>
+//       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+//         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+//           {snippet?.title?.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+//         </Typography>
+//       </Link>
+//       <Link to={snippet?.channelId ? `/channel/${snippet.channelId}` : demoChannelUrl}>
+//         <Typography variant="subtitle2" color="gray">
+//           {snippet?.channelTitle || demoChannelTitle}
+//           <CheckCircleIcon sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+//         </Typography>
+//       </Link>
+//     </CardContent>
+//   </Card>
+// );
+
+// VideoCard.propTypes = {
+//   video: PropTypes.shape({
+//     id: PropTypes.shape({
+//       videoId: PropTypes.string,
+//     }).isRequired,
+//     snippet: PropTypes.shape({
+//       title: PropTypes.string,
+//       thumbnails: PropTypes.shape({
+//         high: PropTypes.shape({
+//           url: PropTypes.string,
+//         }),
+//       }),
+//       channelId: PropTypes.string,
+//       channelTitle: PropTypes.string,
+//     }).isRequired,
+//   }).isRequired,
+// };
+
+// export default VideoCard;
